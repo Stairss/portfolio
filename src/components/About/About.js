5a6c1165606356a4862abe54b58841041752c0b5
Dawid Spisak <dawidspisak@gmail.com>
2021-05-27 01:34:42 +0200

    complete second theme

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,32 @@
 import './About.scss'
 import Submarine from './Submarine'
+import image from '../../assets/unnamed.png'
 const About = () => {
     return (
         <div className="about">
             <Submarine />
+            <div className="about__info">
+
+
+                <header className="about__header">
+                    <h1>About</h1>
+                </header>
+                <section className="about__content">
+                    <div className="about__image">
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="about__description">
+                        <h2>I'm Dawid Spisak</h2>
+                        <p>Enthusiastic Web Developer, Freelancer eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of Javascript, React, Redux and MERN. Motivated to learn, grow and excel in the Dev industry.
+                        </p>
+                    </div>
+                </section>
+            </div>
+
         </div>
     )
 }
 
 export default About
+
+
--- src/components/About/Submarine.js
+++ src/components/About/Submarine.js
@@ -1,20 +1,20 @@
 import './Submarine.scss'
 const Submarine = () => {
     return (
-        <div class="sea">
-            <div class="circle-wrapper">
-                <div class="bubble"></div>
-                <div class="submarine-wrapper">
-                    <div class="submarine-body">
-                        <div class="window"></div>
-                        <div class="engine"></div>
-                        <div class="light"></div>
+        <div className="sea">
+            <div className="circle-wrapper">
+                <div className="bubble"></div>
+                <div className="submarine-wrapper">
+                    <div className="submarine-body">
+                        <div className="window"></div>
+                        <div className="engine"></div>
+                        <div className="light"></div>
                     </div>
-                    <div class="helix"></div>
-                    <div class="hat">
-                        <div class="leds-wrapper">
-                            <div class="periscope"></div>
-                            <div class="leds"></div>
+                    <div className="helix"></div>
+                    <div className="hat">
+                        <div className="leds-wrapper">
+                            <div className="periscope"></div>
+                            <div className="leds"></div>
                         </div>
                     </div>
                 </div>
