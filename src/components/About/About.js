f4bb57ecd9a41ba8b5a8b5775450596240f85740
Dawid Spisak <dawidspisak@gmail.com>
2021-05-27 23:29:10 +0200

    working on About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,32 +1,27 @@
 import './About.scss'
 import Submarine from './Submarine'
 import image from '../../assets/unnamed.png'
+import Octocat from './Octocat'
+import { Wave, Random } from 'react-animated-text';
 const About = () => {
     return (
         <div className="about">
-            <Submarine />
             <div className="about__info">
-
-
-                <header className="about__header">
-                    <h1>About</h1>
-                </header>
-                <section className="about__content">
-                    <div className="about__image">
-                        <img src={image} alt="" />
-                    </div>
-                    <div className="about__description">
-                        <h2>I'm Dawid Spisak</h2>
-                        <p>Enthusiastic Web Developer, Freelancer eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of Javascript, React, Redux and MERN. Motivated to learn, grow and excel in the Dev industry.
-                        </p>
-                    </div>
-                </section>
+                <h1>
+                    <Wave text="I'm Dawid Spisak" effect="stretch" effectChange={2.0} />
+                </h1>
+                <p>Enthusiastic Web Developer, Freelancer eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of Javascript, React, Redux and MERN. Motivated to learn, grow and excel in the Dev industry.
+                </p>
             </div>
-
+            <div className="about__animations">
+                <Octocat />
+                <Submarine />
+            </div>
         </div>
     )
 }
 
 export default About
+
 
 
---
+++ src/components/About/Octocat.js
@@ -0,0 +1,8 @@
+import './Octocat.scss'
+const Octocat = () => {
+    return (
+        <div id="octocat"></div>
+    )
+}
+
+export default Octocat
