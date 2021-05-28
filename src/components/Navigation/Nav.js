469f3b059894081f54c9d6c5edd4ec6e27b4171c
Dawid Spisak <dawidspisak@gmail.com>
2021-05-28 18:38:05 +0200

    working on sliding underline

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,26 +1,34 @@
 import './Nav.scss'
 import { NavLink, Link } from 'react-router-dom';
+import { useState } from 'react';
+
 const Nav = () => {
-    return (
-        <>
-            <nav className="nav">
-                <div className="nav__avatar">
+    const [width, setwWdth] = useState(56)
+    const handleHeight = (e) => {
+        setwWdth(e.currentTarget.offsetWidth)
+    }
+    
+return (
+    <>
+        <nav className="nav">
+            <div className="nav__avatar">
 
-                    <h3><Link to="/">DS</Link></h3>
+                <h3><Link to="/">DS</Link></h3>
 
-                </div>
-                <ul className="nav__list">
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/skills">Skills</NavLink></li>
-                    <li><NavLink to="/projects">Projects</NavLink></li>
-                </ul>
-                <div className="nav__link">
-                    <Link to="/contact">Contact</Link>
-                </div>
-            </nav>
-        </>
-    )
+            </div>
+            <ul className="nav__list">
+                <li><NavLink to="/" onClick={handleHeight}>Home</NavLink></li>
+                <li><NavLink to="/about" onClick={handleHeight} >About</NavLink></li>
+                <li><NavLink to="/skills" onClick={handleHeight}>Skills</NavLink></li>
+                <li><NavLink to="/projects" onClick={handleHeight}>Projects</NavLink></li>
+                <div className="nav__underbar"  style={{width: `${width}px`}}></div>
+            </ul>
+            <div className="nav__link">
+                <Link to="/contact">Contact</Link>
+            </div>
+        </nav>
+    </>
+)
 }
 
 export default Nav
