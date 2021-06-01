9686dc36e23bb167c68ee5a8a3669a62b8e92b7d
Dawid Spisak <dawidspisak@gmail.com>
2021-06-01 12:39:18 +0200

    create Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,42 @@
-import Paralax from './Paralax'
+// import Paralax from './Paralax'
+import { Card, CardContent, CardMedia, makeStyles } from '@material-ui/core'
 import './Projects.scss'
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+      backgroundColor: 'inherit',
+      padding: '20px',
+    },
+    cardMedia: {
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+        // objectFit: 'contain',
+        height: '100%',
+        // width: '100%',
+    },
+    img:{
+        objectFit: 'contain',
+    }
+}));
 const Projects = () => {
+    const classes = useStyles();
     return (
         <div className="projects">
-            <Paralax />
-        <div className="projects__content">
-            
+            {/* <Paralax /> */}
+    <div className="projects__container">
+       <div className="projects__header">
+       <h1>Projects</h1>
+       </div>
+            <div className="projects__content">
+            <Card className={classes.card}>
+            <CardMedia className={classes.cardMedia}
+            image="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png">
+            </CardMedia>
+            <CardContent>
+                {/* <img src="" alt="" /> */}
+                </CardContent>
+            </Card>
+            </div>
         </div>
         </div>
     )
